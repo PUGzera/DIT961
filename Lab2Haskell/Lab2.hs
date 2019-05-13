d34d4503a43efe8cee44a2ff7b9eb671c9efc0db
PUGzera <allecoo1@hotmail.se>
2019-05-14 00:52:00 +0200

    Add files via upload

--- Lab2Haskell/Lab2.hs
+++ Lab2Haskell/Lab2.hs
@@ -3,7 +3,7 @@ import System.Environment
 import System.IO
 import SkewHeap
 import Test.QuickCheck
-
+import Data.Maybe
 -- | Bids.
 
 data Bid
@@ -13,16 +13,14 @@ data Bid
   | NewSell Person Price Price -- Person changes sell bid
   deriving Show
 
-data BuyBid = BuyBid Person Price | NoBuyBid
-data SellBid = SellBid Person Price | NoSellBid
+data BuyBid = BuyBid Person Price
+data SellBid = SellBid Person Price
 
 
 instance Show BuyBid where
-  show NoBuyBid = ""
   show (BuyBid n p) = n ++ " " ++ show p
 
 instance Show SellBid where
-  show NoSellBid = ""
   show (SellBid n p) = n ++ " " ++ show p 
 
 instance Eq BuyBid where
@@ -35,10 +33,7 @@ instance Ord SellBid where
   compare (SellBid _ p) (SellBid _ p') = compare p p'
 
 instance Ord BuyBid where
-  (>=) (BuyBid _ p) (BuyBid _ p') = p <= p'
-  (>) (BuyBid _ p) (BuyBid _ p') = p < p'
   (<=) (BuyBid _ p) (BuyBid _ p') = p >= p'
-  (<) (BuyBid _ p) (BuyBid _ p') = p > p'
 
 
 
@@ -102,49 +97,55 @@ main = do
 -- | The core of the program. Takes a list of bids and executes them.
 
 trade :: [Bid] -> IO ()
-trade bs = trade' (OrderBook Leaf Leaf) bs
+trade bs = trade' bs (OrderBook Leaf Leaf)
     where
-      trade' :: OrderBook -> [Bid] -> IO()
-      trade' (OrderBook Leaf Leaf) []   = print "No transactions"
-      trade' orderbook []               = print orderbook
-      trade' (OrderBook bs ss) (x:xs) = case x of
-        (Buy name price) -> do
-                            let s = case (extractMin ss) of
-                                      Just n  -> n
-                                      Nothing -> NoSellBid
-                            let b = (BuyBid name price)
-                            attemptPurchase b s (trade' (OrderBook bs (remove ss)) xs) (trade' (OrderBook (insert bs (BuyBid name price)) ss) xs)   
-        (Sell name price) -> do
-                            let b = case (extractMin bs) of
-                                      Just n  -> n
-                                      Nothing -> NoBuyBid
-                            let s = (SellBid name price)
-                            attemptPurchase b s (trade' (OrderBook (remove bs) ss) xs) (trade' (OrderBook bs (insert ss (SellBid name price))) xs)                      
-        (NewBuy name oldPrice newPrice) -> do
-                                            let s = case (extractMin ss) of
-                                                      Just n  -> n
-                                                      Nothing -> NoSellBid
+      trade' :: [Bid] -> OrderBook -> IO()
+      trade' [] o     = print o
+      trade' (x:xs) o@(OrderBook bs ss) = case x of
+        b@(Buy _ _) -> trade' xs =<< (processTrade b o)
+        s@(Sell _ _) -> trade' xs =<< (processTrade s o)
+        (NewSell n op np) -> do
+          (OrderBook bs ss) <- processTrade (Sell n np) o
+          trade' xs (OrderBook bs (removeVal ss (SellBid n op)))
+        (NewBuy n op np) -> do
+          (OrderBook bs ss) <- processTrade (Buy n np) o
+          trade' xs (OrderBook (removeVal bs (BuyBid n op)) ss)
+        {--(NewBuy name oldPrice newPrice) -> do
+                                            let ons = (trade' xs (OrderBook (removeVal bs (BuyBid name oldPrice)) (remove ss)))
+                                            let onf = (trade' xs (OrderBook (insert (removeVal bs (BuyBid name oldPrice)) (BuyBid name newPrice)) ss))
                                             let b = (BuyBid name newPrice)
-                                            attemptPurchase b s (trade' (OrderBook (removeVal bs (BuyBid name oldPrice)) (remove ss)) xs) (trade' (OrderBook (insert (removeVal bs (BuyBid name oldPrice)) (BuyBid name newPrice)) ss) xs) 
+                                            case (extractMin ss) of
+                                                  Just s -> attemptPurchase b s ons onf
+                                                  Nothing -> onf 
         (NewSell name oldPrice newPrice) -> do
-                                            let b = case (extractMin bs) of
-                                                      Just n  -> n
-                                                      Nothing -> NoBuyBid
-                                            let s = (SellBid name newPrice) 
-                                            attemptPurchase b s (trade' (OrderBook (remove bs) (removeVal ss (SellBid name oldPrice))) xs) (trade' (OrderBook bs (insert (removeVal ss (SellBid name oldPrice)) (SellBid name newPrice))) xs)   
-                                                              
+                                            let ons = (trade' xs (OrderBook (remove bs) (removeVal ss (SellBid name oldPrice))))
+                                            let onf = (trade' xs (OrderBook bs (insert (removeVal ss (SellBid name oldPrice)) (SellBid name newPrice))))
+                                            let s = (SellBid name newPrice)
+                                            case (extractMin bs) of
+                                                  Just b -> attemptPurchase b s ons onf
+                                                  Nothing -> onf--}
 
+printTransaction :: (Person, Person) -> Price -> IO ()
+printTransaction (n, n') p = putStrLn $ n ++ " buys from " ++ n' ++ " for " ++ show p ++ "kr"
+      
+processTrade :: Bid -> OrderBook -> IO OrderBook
+processTrade (Buy n p) (OrderBook bs ss) | isJust (extractMin ss) && p >= (price $ fromJust min) = do
+                                            let (SellBid n' _) = fromJust $ min
+                                            printTransaction (n, n') p  
+                                            return $ OrderBook bs (remove ss)
+                                         | otherwise = return $  OrderBook (insert bs (BuyBid n p)) ss
+                                          where
+                                            min = extractMin ss
+                                            price (SellBid _ p) = p
+processTrade (Sell n p) (OrderBook bs ss) | isJust (extractMin bs) && p <= (price $ fromJust min) = do
+                                            let (BuyBid n' _) = fromJust $ min
+                                            printTransaction (n, n') p  
+                                            return $ OrderBook (remove bs) ss
+                                          | otherwise = return $ OrderBook bs (insert ss (SellBid n p))
+                                           where
+                                             min = extractMin bs
+                                             price (BuyBid _ p) = p 
 
-attemptPurchase :: BuyBid -> SellBid -> IO() -> IO() -> IO()
-attemptPurchase NoBuyBid _ _ onFailure = onFailure
-attemptPurchase _ NoSellBid _ onFailure = onFailure
-attemptPurchase (BuyBid name p) (SellBid name' p') onSuccess onFailure = 
-  do
-    if p >= p' then do
-      putStrLn $ name ++ " buys from " ++ name' ++ " for " ++ show p ++ "kr"
-      onSuccess
-    else do     
-      onFailure
 
 
 --testing      
--- Lab2Haskell/SkewHeap.hs
+++ Lab2Haskell/SkewHeap.hs
@@ -2,6 +2,7 @@ module SkewHeap (SkewHeap (Leaf, Branch), merge, insert, extractMin, remove, isE
 
 
 import Test.QuickCheck    
+import Data.Maybe
 
 
 data SkewHeap a = Leaf | Branch (SkewHeap a) a (SkewHeap a)
@@ -37,9 +38,17 @@ insert :: Ord a => SkewHeap a -> a -> SkewHeap a
 insert t a = t <> (Branch Leaf a Leaf)
 
 
+removeVal :: Ord a => SkewHeap a -> a -> SkewHeap a
+removeVal t a = removeVal' t Leaf a
+                    where
+                        removeVal' Leaf _ _ = Leaf
+                        removeVal' t@(Branch l n r) t' a | n == a    = merge (merge l r) t'
+                                                         | otherwise = removeVal' (remove t) (insert t' (fromJust $ extractMin t)) a  
+
+
 extractMin :: Ord a => SkewHeap a -> Maybe a
 extractMin Leaf             = Nothing
-extractMin t@(Branch _ n _) = Just n
+extractMin (Branch _ n _)   = Just n
 
 
 remove :: Ord a => SkewHeap a -> SkewHeap a
@@ -47,44 +56,6 @@ remove Leaf           = Leaf
 remove (Branch l n r) = l <> r
 
 
-removeVal :: Ord a => SkewHeap a -> a -> SkewHeap a
-removeVal Leaf _ = Leaf
-removeVal heap a = removeVal' [] heap a
-    where
-        removeVal' :: Ord a => [a] -> SkewHeap a -> a -> SkewHeap a
-        removeVal' xs h@(Branch l n r) a | n == a    = (remove h) <> (fromList xs)
-                                         | otherwise = removeVal' (n:xs) (remove h) a
-
-fromList :: Ord a => [a] -> SkewHeap a
-fromList [] = Leaf
-fromList xs = fromList' xs Leaf
-        where
-            fromList' :: Ord a => [a] -> SkewHeap a -> SkewHeap a
-            fromList' [] heap     = heap
-            fromList' (x:xs) heap = fromList' xs (insert heap x)
-
-toList :: Ord a => SkewHeap a -> [a]
-toList Leaf = []
-toList heap = toList' heap []
-    where
-        toList' :: Ord a => SkewHeap a -> [a] -> [a]
-        toList' Leaf xs = xs
-        toList' heap xs = toList' (remove heap) ((min):xs)
-            where
-                Just min = extractMin heap
-
-isValid :: Ord a => SkewHeap a -> Bool
-isValid heap = valid' $ toList heap
-        where
-            valid' :: Ord a => [a] -> Bool
-            valid' []       = True
-            valid' [x]      = True
-            valid' (x:y:xs) = x >= y && valid' (y:xs)
-
 isEmpty :: Ord a => SkewHeap a -> Bool
 isEmpty Leaf = True
 isEmpty _    = False
-
-
-heap_prop :: [Int] -> [Int] -> Bool
-heap_prop xs ys = length (toList $ (fromList xs) <> (fromList ys)) == length (xs ++ ys)
